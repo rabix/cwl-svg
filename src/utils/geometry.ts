0890b748f66b210e734c37fb68e80c32b404d290
Maja Nedeljković <maya.nedeljko@gmail.com>
2017-11-17 15:34:16 +0100

    getTransformToElement doesn't depend on getScreenCTM

--- src/utils/geometry.ts
+++ src/utils/geometry.ts
@@ -6,6 +6,29 @@ export class Geometry {
     }
 
     static getTransformToElement(from, to) {
-        return to.getScreenCTM().inverse().multiply(from.getScreenCTM());
+        const getPosition = (node: SVGGElement, addE = 0, addF = 0): SVGMatrix => {
+
+            if (!node.ownerSVGElement) {
+                // node is the root svg element
+                const matrix = (node as SVGSVGElement).createSVGMatrix();
+                matrix.e = addE;
+                matrix.f = addF;
+                return matrix;
+            } else {
+                // node still has parent elements
+                const {e, f} = node.transform.baseVal.getItem(0).matrix;
+
+                return getPosition(<SVGGElement>node.parentNode, e + addE, f + addF);
+            }
+        };
+
+        const toPosition = getPosition(to);
+        const fromPosition = getPosition(from);
+
+        const result = from.ownerSVGElement.createSVGMatrix();
+        result.e = toPosition.e - fromPosition.e;
+        result.f = toPosition.f - fromPosition.f;
+
+        return result.inverse();
     }
 }
